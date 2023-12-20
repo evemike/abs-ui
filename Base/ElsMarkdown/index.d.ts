99c1f75bb249f4324d37e406dd0487df80d9122e
mike-work <evemike@163.com>
2023-12-20 17:03:56 +0800

    优化~

--- Base/ElsMarkdown/index.d.ts
+++ Base/ElsMarkdown/index.d.ts
@@ -1,10 +1,8 @@
 import { App } from 'vue';
-import ElsMarkdown from "./main.vue";
 import ElsMarkdownView from "./view.vue";
-import ElsMarkdownEditor from "./editor.vue";
 declare const _default: {
     install: (app: App) => void;
 };
 export default _default;
-export { ElsMarkdown, ElsMarkdownView, ElsMarkdownEditor };
+export { ElsMarkdownView };
 export * from "./inter";
