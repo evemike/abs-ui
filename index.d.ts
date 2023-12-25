1ccc6f275a90725524efd4e6f786d93c380828f1
mike-work <evemike@163.com>
2023-12-25 17:09:08 +0800

    优化~

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,6 @@
-import { App } from 'vue';
+import type { App } from 'vue';
 declare const _default: {
     install: (app: App) => void;
 };
 export default _default;
-export * from "./Base/ElsElem";
-export * from "./Base/ElsForm";
-export * from "./Base/ElsX6";
 export * from "./Base/ElsMarkdown";
-export * from "./Base/CodeEditor";
-export * from "./Base/JsonView";
